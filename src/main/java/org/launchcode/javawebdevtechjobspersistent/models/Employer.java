cac3c0b936ed74ad597190fd152433c5fb1eefc1
adamdotg <adamdotg@gmail.com>
2020-03-05 13:35:17 -0600

    edits to Employer and Skill models

--- src/main/java/org/launchcode/javawebdevtechjobspersistent/models/Employer.java
+++ src/main/java/org/launchcode/javawebdevtechjobspersistent/models/Employer.java
@@ -3,15 +3,16 @@ package org.launchcode.javawebdevtechjobspersistent.models;
 import javax.persistence.Entity;
 import javax.validation.constraints.Max;
 import javax.validation.constraints.NotBlank;
+import javax.validation.constraints.Size;
 
 @Entity
 public class Employer extends AbstractEntity {
 
     @NotBlank(message = "Location is required")
-    @Max(value = 50, message = "Location must be less than 50 characters")
+    @Size(max = 50, message = "Location must be less than 50 characters")
     private String location;
 
-    public Employer(@NotBlank(message = "Location is required") @Max(value = 50, message = "Location must be less than 50 characters") String location) {
+    public Employer(@NotBlank(message = "Location is required") @Size(max = 50, message = "Location must be less than 50 characters") String location) {
         this.location = location;
     }
 
--- src/main/java/org/launchcode/javawebdevtechjobspersistent/models/Skill.java
+++ src/main/java/org/launchcode/javawebdevtechjobspersistent/models/Skill.java
@@ -3,15 +3,16 @@ package org.launchcode.javawebdevtechjobspersistent.models;
 import javax.persistence.Entity;
 import javax.validation.constraints.Max;
 import javax.validation.constraints.NotBlank;
+import javax.validation.constraints.Size;
 
 @Entity
 public class Skill extends AbstractEntity {
 
     @NotBlank(message = "Description must not be empty")
-    @Max(value = 500, message = "Description must not exceed 500 characters")
+    @Size(max = 500, message = "Description must not exceed 500 characters")
     private String description;
 
-    public Skill(@NotBlank(message = "Description must not be empty") @Max(value = 500, message = "Description must not exceed 500 characters") String description) {
+    public Skill(@NotBlank(message = "Description must not be empty") @Size(max = 500, message = "Description must not exceed 500 characters") String description) {
         this.description = description;
     }
 
