058504b66af71bfda66b54fae595dee0dd4dcb7c
adamdotg <adamdotg@gmail.com>
2020-03-04 23:38:24 -0600

    Added fixes to JobData and ListController

--- src/main/java/org/launchcode/javawebdevtechjobspersistent/controllers/ListController.java
+++ src/main/java/org/launchcode/javawebdevtechjobspersistent/controllers/ListController.java
@@ -28,8 +28,8 @@ public class ListController {
 
         columnChoices.put("all", "All");
         columnChoices.put("employer", "Employer");
-        columnChoices.put("skills", "Skills");
-
+        columnChoices.put("skill", "Skill");
+//      columnChoices.put("skills", "Skills");
     }
 
     @RequestMapping("")
