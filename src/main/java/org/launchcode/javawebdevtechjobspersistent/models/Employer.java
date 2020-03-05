bf216d89bdab8649e37de33b08539c2316adea4e
adamdotg <adamdotg@gmail.com>
2020-03-04 23:17:23 -0600

    Employer and Skill models edited

--- src/main/java/org/launchcode/javawebdevtechjobspersistent/models/Employer.java
+++ src/main/java/org/launchcode/javawebdevtechjobspersistent/models/Employer.java
@@ -1,6 +1,30 @@
 package org.launchcode.javawebdevtechjobspersistent.models;
 
+import javax.persistence.Entity;
+import javax.validation.constraints.Max;
+import javax.validation.constraints.NotBlank;
+
+@Entity
 public class Employer extends AbstractEntity {
 
+    @NotBlank(message = "Location is required")
+    @Max(value = 50, message = "Location must be less than 50 characters")
+    private String location;
 
+    public Employer(@NotBlank(message = "Location is required") @Max(value = 50, message = "Location must be less than 50 characters") String location) {
+        this.location = location;
+    }
+
+    public Employer () {}
+
+    public String getLocation() {
+        return location;
+    }
+
+    public void setLocation(String location) {
+        this.location = location;
+    }
+//    Is this needed?
+//    @Override
+//    public String toString() {return location;}
 }
--- src/main/java/org/launchcode/javawebdevtechjobspersistent/models/Skill.java
+++ src/main/java/org/launchcode/javawebdevtechjobspersistent/models/Skill.java
@@ -1,5 +1,27 @@
 package org.launchcode.javawebdevtechjobspersistent.models;
 
+import javax.persistence.Entity;
+import javax.validation.constraints.Max;
+import javax.validation.constraints.NotBlank;
+
+@Entity
 public class Skill extends AbstractEntity {
 
+    @NotBlank(message = "Description must not be empty")
+    @Max(value = 500, message = "Description must not exceed 500 characters")
+    private String description;
+
+    public Skill(@NotBlank(message = "Description must not be empty") @Max(value = 500, message = "Description must not exceed 500 characters") String description) {
+        this.description = description;
+    }
+
+    public Skill () {}
+
+    public String getDescription() {
+        return description;
+    }
+
+    public void setDescription(String description) {
+        this.description = description;
+    }
 }
