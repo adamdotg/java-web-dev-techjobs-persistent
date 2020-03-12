a64b6b1f35bab390c684cc1bfef753487cf3c543
adamdotg <adamdotg@gmail.com>
2020-03-11 20:26:36 -0500

    updated Job model

--- src/main/java/org/launchcode/javawebdevtechjobspersistent/models/Job.java
+++ src/main/java/org/launchcode/javawebdevtechjobspersistent/models/Job.java
@@ -5,23 +5,40 @@ import javax.persistence.*;
 @Entity
 public class Job extends AbstractEntity{
 
-    private String employer;
+    @ManyToOne
+    private Employer employer;
+
     private String skills;
 
     public Job() {
     }
 
-    public Job(String anEmployer, String someSkills) {
-        super();
+//    public Job(String anEmployer, String someSkills) {
+//        super();
+//        this.employer = anEmployer;
+//        this.skills = someSkills;
+//    }
+
+
+    public Job(Employer anEmployer, String someSkills) {
         this.employer = anEmployer;
         this.skills = someSkills;
     }
 
-    public String getEmployer() {
+//    public String getEmployer() {
+//        return employer;
+//    }
+//
+//    public void setEmployer(String employer) {
+//        this.employer = employer;
+//    }
+
+
+    public Employer getEmployer() {
         return employer;
     }
 
-    public void setEmployer(String employer) {
+    public void setEmployer(Employer employer) {
         this.employer = employer;
     }
 
