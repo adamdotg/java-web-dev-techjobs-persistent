d2221d783c5d01f4bab4aa23d1469b05aea55ae9
adamdotg <adamdotg@gmail.com>
2020-04-01 15:44:41 -0500

    added jobs field

--- src/main/java/org/launchcode/javawebdevtechjobspersistent/models/Skill.java
+++ src/main/java/org/launchcode/javawebdevtechjobspersistent/models/Skill.java
@@ -1,9 +1,12 @@
 package org.launchcode.javawebdevtechjobspersistent.models;
 
 import javax.persistence.Entity;
+import javax.persistence.ManyToMany;
 import javax.validation.constraints.Max;
 import javax.validation.constraints.NotBlank;
 import javax.validation.constraints.Size;
+import java.util.ArrayList;
+import java.util.List;
 
 @Entity
 public class Skill extends AbstractEntity {
@@ -15,6 +18,9 @@ public class Skill extends AbstractEntity {
     public Skill(@NotBlank(message = "Description must not be empty") @Size(max = 500, message = "Description must not exceed 500 characters") String description) {
         this.description = description;
     }
+
+    @ManyToMany(mappedBy = "skills")
+    private final List<Job> jobs = new ArrayList<>();
 
     public Skill () {}
 
