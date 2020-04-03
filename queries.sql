10ce3cbec913dfd49291aef9334a2a57e99eb138
adamdotg <adamdotg@gmail.com>
2020-04-03 18:08:17 -0500

    added sql query part 3

--- queries.sql
+++ queries.sql
@@ -3,5 +3,5 @@ show fields from techjobs.job;
 ## Part 2: Test it with SQL
 SELECT name from employer where location = "St. Louis City";
 ## Part 3: Test it with SQL
-
+Drop table job;
 ## Part 4: Test it with SQL
